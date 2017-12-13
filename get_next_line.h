83a11bc3066b23e303a0f0225cd59797f9af4778
kmckee <kmckee@student.42.fr>
2017-12-13 10:52:51 -0800

    intital commit of all GNL files

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmckee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/20 20:44:36 by kmckee            #+#    #+#             */
+/*   Updated: 2017/11/03 15:17:12 by kmckee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
+# define FAIL(x) if (x) return -1
+
+int	get_next_line(const int fd, char **line);
+
+#endif
